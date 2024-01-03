111e4f7826819398fec011e07655e1870ac60a82
Ve Maricic <ve@datora.de>
2024-01-03 01:46:48 +0100

    newest highscore highlighted fixed

--- gameplay/src/components/Highscores.js
+++ gameplay/src/components/Highscores.js
@@ -5,19 +5,17 @@ function Highscores({ newScore, scores, newScoreId }) {
     <div className="highscores-container">
       <h2>High Scores</h2>
       <table className="highscores-table">
-        <thead>
-          <tr>
-            <th id="header-name">Name</th>
-            <th id="header-score">Score</th>
-          </tr>
-        </thead>
+        <th id="header-name">Player</th>
+        <th id="header-score">Score</th>
         <tbody>
           {scores
             .sort((a, b) => b.score - a.score)
             .map((score, index) => (
               <tr
                 key={score.id}
-                className={newScoreId === score.id ? "highlighted" : ""}
+                className={`${newScoreId === score.id ? "highlighted" : ""} ${
+                  index % 2 === 0 ? "even" : "odd"
+                }`}
               >
                 <td className="player-name">{score.player_name}</td>
                 <td className="player-score">{score.score}</td>
